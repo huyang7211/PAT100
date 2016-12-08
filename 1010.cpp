1de7a089550a7ab38c206a5092846cf9c3cb86a3
huyang7211 <huyang_a@126.com>
2016-12-08 21:26:05 +0800

    一元多项式求导，有改进，但是还有一个测试点
    添加了f(x) = 0和 f(x) = nx的情况，但是还是有一个测试点不能通过

--- 1010.cpp
+++ 1010.cpp
@@ -7,17 +7,36 @@ int main1010()
 	vector<int> vecOutput;
 	int n = 0;
 	int i = 0;
-	while(cin>>n && getchar()!='\n')//加一个判断，当回车时结束输入
+	while(cin>>n)//加一个判断，当回车时结束输入
 	{
 		vecInput.push_back(n);
+		if(getchar() == '\n')
+			break;
 	}
 	int size = vecInput.size();
-
-	for(i = 0;i < size-2; i=i+2)
+	if(size == 2)
 	{
-		vecOutput.push_back(multi(vecInput[i],vecInput[i+1]));
-		vecOutput.push_back(vecInput[i+1] - 1);
+		if(vecInput[1] == 0)//当输入为f(x) = 0时的特殊情况 modified by kangshn 1208
+		{
+			vecOutput.push_back(multi(vecInput[0],vecInput[1]));
+			vecOutput.push_back(vecInput[1]);
+		}
+		else//当输入为f(x) = nx时的特殊情况 modified by kangshn 1208
+		{
+			vecOutput.push_back(multi(vecInput[0],vecInput[1]));
+			vecOutput.push_back(vecInput[1] - 1);
+		}
+		
 	}
+	else
+	{
+		for(i = 0;i < size - 2; i=i+2)//对于输入的参数多于两个的时候有效
+		{
+			vecOutput.push_back(multi(vecInput[i],vecInput[i+1]));
+			vecOutput.push_back(vecInput[i+1] - 1);
+		}
+	}
+	
 	for(i = 0;i<vecOutput.size();++i)
 	{
 		cout<<vecOutput[i];
